abfbd5cf7e6a8410924b516002f787a789ef98ec
Thi Tinh <thitrungtinh@gmail.com>
2024-10-18 21:43:13 +0700

    sửa lại return cho function checkout

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,44 +7,47 @@ use App\Models\OrderDetail;
 use App\Models\Voucher;
 use Illuminate\Http\Request;
 
-class OrderController extends Controller
+class OrderController extends BaseController
 {
     public function checkout(Request $request)
     {
-        $cart = $request->cart;
+        try {
+            $cart = $request->cart;
 
-
-        if (!$cart || count($cart) === 0) {
-            return response()->json(['success' => false, 'message' => 'Giỏ hàng trống!']);
-        }
-        // Kiểm tra voucher (nếu có)
-        $voucher = null;
-        if ($request->voucher_id) {
-            $voucher = Voucher::find($request->voucher_id);
-            if (!$voucher || !$voucher->is_active) {
-                return response()->json(['success' => false, 'message' => 'Voucher không hợp lệ hoặc đã hết hạn!']);
+            if (!$cart || count($cart) === 0) {
+                return $this->sendError('Giỏ hàng trống!');
             }
-        }
-        // Tạo đơn hàng
-        $order = new Order();
-        $order->user_id = auth()->user()->id;
-        $order->voucher_id = $request->voucher_id ?? null;
-        $order->total_amount = array_sum(array_map(function ($item) {
-            return $item['price'] * $item['quantity'];
-        }, $cart));
-        $order->status_id = 1;
-        $order->save();
-        // Lưu từng sản phẩm vào bảng order_items
-        foreach ($cart as $item) {
-            OrderDetail::create([
-                'order_id' => $order->id,
-                'product_id' => $item['id'],
-                'quantity' => $item['quantity'],
-                'price' => $item['price'],
-                'unit' => $item['unit'],
-            ]);
-        }
+            // Kiểm tra voucher (nếu có)
+            $voucher = null;
+            if ($request->voucher_id) {
+                $voucher = Voucher::find($request->voucher_id);
+                if (!$voucher || !$voucher->is_active) {
+                    return $this->sendError('Voucher không hợp lệ hoặc đã hết hạn!', '', 400);
+                }
+            }
+            // Tạo đơn hàng
+            $order = new Order();
+            $order->user_id = auth()->user()->id;
+            $order->voucher_id = $request->voucher_id ?? null;
+            $order->total_amount = array_sum(array_map(function ($item) {
+                return $item['price'] * $item['quantity'];
+            }, $cart));
+            $order->status_id = 1;
+            $order->save();
+            // Lưu từng sản phẩm vào bảng order_items
+            foreach ($cart as $item) {
+                OrderDetail::create([
+                    'order_id' => $order->id,
+                    'product_id' => $item['id'],
+                    'quantity' => $item['quantity'],
+                    'price' => $item['price'],
+                    'unit' => $item['unit'],
+                ]);
+            }
 
-        return response()->json(['success' => true, 'message' => 'Đặt hàng thành công!']);
+            return $this->sendResponse($order, 'Đặt hàng thành công!');
+        } catch (\Throwable $th) {
+            return $this->sendError('Lỗi định dạng.', ['error' => $th->getMessage()], 404);
+        }
     }
 }
